7ff5f7be591b88e95f7c20e3c1e648bb57eff8fb
Denny Rubino <rubidenny@gmail.com>
2025-01-28 20:14:56 +0100

    add main

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,47 @@ export default function Main() {
           ---{">"}Content goes here {"<"}---
         </p>
       </div>
-      <div className="main-container-2">ciao</div>
+      <div className="main-container-2">
+        <nav className="main-nav">
+          <ul>
+            <li>
+              <img
+                src="public/img/buy-comics-digital-comics.png"
+                className="main-img "
+              ></img>
+              DIGITAL COMICS
+            </li>
+            <li>
+              <img
+                src="public/img/buy-comics-merchandise.png"
+                className="main-img "
+              ></img>
+              DC MERCHANDISE
+            </li>
+            <li>
+              <img
+                src="public/img/buy-comics-subscriptions.png"
+                className="main-img "
+              ></img>
+              SUBSCRIPTION
+            </li>
+            <li>
+              <img
+                src="public/img/buy-comics-shop-locator.png"
+                className="main-img "
+              ></img>
+              COMIC SHOP LOCATOR
+            </li>
+            <li>
+              <img
+                scr="public/img/buy-dc-power-visa.svg"
+                className="main-img "
+              ></img>
+              DC POWER VISA
+            </li>
+          </ul>
+        </nav>
+      </div>
     </main>
   );
 }
