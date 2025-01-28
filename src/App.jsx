d6993a4dbec43d1b76382f38cff8defe80bcc5a3
Denny Rubino <rubidenny@gmail.com>
2025-01-28 17:10:27 +0100

    fix project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/Header";
+import Main from "./assets/Main";
+import Footer from "./assets/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/assets/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <Header classname="header-Container"></Header>;
+}
